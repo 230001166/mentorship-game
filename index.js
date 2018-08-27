587bc2eb3a1f98c520537ee4b77c88cfb8d07db2
Heather <heather.eichman1@gmail.com>
2018-08-27 14:43:13 -0400

    not commit

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ express()
 
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({ port: 443 });
+const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on("connection", function connection(ws) {
   ws.on("message", function incoming(message) {
