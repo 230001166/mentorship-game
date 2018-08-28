2b9599fe2f3dcef1ec551287ad33691ce391cd6a
Heather <heather.eichman1@gmail.com>
2018-08-28 11:50:16 -0400

    testing server

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ const wss = new WebSocket.Server({ port: 8080 });
 wss.on("connection", function connection(ws) {
   ws.on("message", function incoming(message) {
     console.log("received: %s", message);
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send('data');
+      }
+    });
   });
 
   ws.send("something");
