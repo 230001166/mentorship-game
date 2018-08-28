27ff22e24628863c1cb8a75e7dedd08c6f2348b7
Heather <heather.eichman1@gmail.com>
2018-08-28 14:39:58 -0400

    World data object:

---
+++ worldData.js
@@ -0,0 +1,71 @@
+let worldData = [
+    players = [
+        {
+            name: ,
+            health: ,
+            maxHealth: ,
+            mana: ,
+            maxMana: ,
+            stamina: ,
+            maxStamina: ,
+            strength: ,
+            defense: ,
+            criticalHitChanceOutOf100: ,
+            position: {row: , col: },
+            level: ,
+            experiencePoints: ,
+            equipment: {weapon: , armor: , accessory: }
+        }
+    ],
+
+    enemies = [
+        {
+            name: ,
+            health: ,
+            maxHealth: ,
+            mana: ,
+            maxMana: ,
+            stamina: ,
+            maxStamina: ,
+            strength: ,
+            defense: ,
+            criticalHitChanceOutOf100: ,
+            position: {row: , col: },
+            experiencePoints: ,
+            drops: {
+                drop: {
+                    indentifier: ,
+                    dropChanceOutOf100:
+                }
+            }
+        }
+        
+    ],
+
+    worldItems = [
+        item: {
+            identifier: ,
+            position: {row: , col: }
+        }
+    ],
+
+    gameItems = [
+        item: {
+            name: ,
+            attackValue: ,
+            defenseValue: ,
+            healthValue: ,
+            ManaValue: ,
+            criticalHitBonus: ,
+            equipmentType: ,
+            description:
+        }
+    ],
+
+    worldMap = [
+        tile: {
+            position: {row: , col: },
+            description:
+        }
+    ]
+];
