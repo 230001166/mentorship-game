64303343ad06dc9fca429a9df4e24e13ea521ff0
Dorian Voegeli <voegeli.dorian@gmail.com>
2018-08-30 11:20:07 -0400

    change server code for heroku

--- index.js
+++ index.js
@@ -1,42 +1,24 @@
-const cool = require("cool-ascii-faces");
-const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 5000;
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
 
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
-
-const WebSocket = require("ws");
-
-const wss = new WebSocket.Server({ port: 8080 });
-
-express()
-  .use(express.static(path.join(__dirname, "public")))
-  .set("views", path.join(__dirname, "views"))
-  .set("view engine", "ejs")
-  .get("/", (req, res) => res.render("pages/index"))
-  .get("/cool", (req, res) => res.send(cool()))
-  .get("/times", (req, res) => {
-    let result = "";
-    const times = process.env.TIMES || 5;
-    for (i = 0; i < times; i++) {
-      result += i + " ";
-    }
-    res.send(result);
-  })
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-
-
-wss.on("connection", function connection(ws) {
-  ws.on("message", function incoming(message) {
-    console.log("received: %s", message);
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send('data');
-      }
-    });
-
+ 
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
   });
+  wss.clients.forEach(function each(client) {
+    if (client !== ws && client.readyState === WebSocket.OPEN) {
+      client.send("tada!");
+    }
+  });
+  ws.send('something');
+});
 
-  ws.send("something");
+server.listen(8080, () => {
+  //console.log(`Listening on ${server.address().port}`);
 });
