67a534f07bda27145b245a5482c5720cb53401fe
230001166 <dpplasma@gmail.com>
2018-08-30 15:11:39 -0400

    added worlddata json

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const cool = require("cool-ascii-faces");
-
 const express = require("express");
-
 const path = require("path");
-
 const PORT = process.env.PORT || 5000;
-
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({ port: 8080 });
-
-express()
+const server = express()
   .use(express.static(path.join(__dirname, "public")))
   .set("views", path.join(__dirname, "views"))
   .set("view engine", "ejs")
@@ -26,10 +20,31 @@ express()
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+const wss = new WebSocket.Server ({ server});
+
 wss.on("connection", function connection(ws) {
   ws.on("message", function incoming(message) {
+
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+
     console.log("received: %s", message);
   });
 
+  ws.on("close", function() {
+    console.log("websocket connection close")
+    clearInterval(id)
+  })
+
   ws.send("something");
 });
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);
+
