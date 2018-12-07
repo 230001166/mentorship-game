5ff6843cebcb42d4a965c91f2cc7215897a9618c
230001166 <dpplasma@gmail.com>
2018-12-07 17:35:01 -0500

    Added button to display most recent server messages.

--- server.js
+++ server.js
@@ -959,9 +959,12 @@ wss.on("connection", function connection(ws, req) {
 });
 
 function sendServerMessage(client, messageType, messageName) {
+
+  let date = new Date ();
   let message = {
     messageType: messageType,
-    text: messageName
+    text: messageName,
+    date: date.toString ()
   };
 
   client.send(JSON.stringify(message));
